67c05ae6874077b23d700b65364492bedd340648
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-10 21:08:08 +0100

    ADD: Initial node and mocha setup

---
+++ test/scrape.js
@@ -0,0 +1,13 @@
+var should = require('should');
+
+suite('onthemarket', function() {
+    setup(function() {
+
+    });
+
+    suite('#scrape()', function() {
+        test('\'initial test\' should be a string', function() {
+            'initial test'.should.be.a.String();
+        });
+    });
+});
