e7191a7bff7d7dc863d52fb67c39067782fd412f
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 10:43:36 +0100

    ADD: Test for only using the type param for createTypeSegement

--- test/scrape.js
+++ test/scrape.js
@@ -4,13 +4,27 @@ var OnTheMarket = require('../src/onthemarket.js');
 
 describe('OnTheMarket', function() {
 
-    it('Create the correct bedroom and type segment', function(){
-        var testApp = new OnTheMarket("Birmingham", 30000, 100000, "house", 3);
+    it('Create the correct type segment using only the type parameters', function(){
+        // Create test app
+        var testAppNoBedrooms = new OnTheMarket("Birmingham", 30000, 100000, "flats-and-apartment", 0);
+        // Call the createTypeSegement();
+        var resultNoBedrooms = testAppNoBedrooms.createTypeSegment();
 
+        // Assert result
+        resultNoBedrooms.should.equal('flats-and-apartment');
+
+    });
+
+    it('Create the correct type segment using both min bedroom and type parameters', function(){
+        // Create test app
+        var testApp = new OnTheMarket("Birmingham", 30000, 100000, "house", 3);
+        // Call the createTypeSegement();
         var result = testApp.createTypeSegment();
 
+        // Assert result
         result.should.equal('3-bed-house');
     });
+
     //
     // it('Create the correct type segment', function(){
     //     false.should.be.True();
