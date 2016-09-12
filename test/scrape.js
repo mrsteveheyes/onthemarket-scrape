fad0c268dfc5b5725aced8c740f5030cc7261216
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 10:33:58 +0100

    ADD: Test for creating type segment

--- test/scrape.js
+++ test/scrape.js
@@ -5,19 +5,23 @@ var OnTheMarket = require('../src/onthemarket.js');
 describe('OnTheMarket', function() {
 
     it('Create the correct bedroom and type segment', function(){
-        false.should.be.True();
-    });
+        var testApp = new OnTheMarket("Birmingham", 30000, 100000, "house", 3);
 
-    it('Create the correct type segment', function(){
-        false.should.be.True();
-    });
+        var result = testApp.createTypeSegment();
 
-    it('Creates the correct URL to call', function(){
-        false.should.be.True();
+        result.should.equal('3-bed-house');
     });
-
-    it('Call the page and return the correct JSON object', function(){
-        false.should.be.True();
-    });
+    //
+    // it('Create the correct type segment', function(){
+    //     false.should.be.True();
+    // });
+    //
+    // it('Creates the correct URL to call', function(){
+    //     false.should.be.True();
+    // });
+    //
+    // it('Call the page and return the correct JSON object', function(){
+    //     false.should.be.True();
+    // });
 
 });
