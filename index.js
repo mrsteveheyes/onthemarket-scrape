ea343076606f4a5bc307e3d1bba981ed97a16cbe
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 20:58:43 +0100

    ADD: Tidy up the main file

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 var OnTheMarket = require('./src/onthemarket.js');
 
 // Remove node and file path
-var params = process.argv.slice(2);
+var params = process.argv.slice(2),
+    app = new OnTheMarket(params[0], params[1], params[2], params[3], params[4]);
 
-var app = new OnTheMarket(params);
-
-console.log(OnTheMarket);
-
+// Run the app
+app
+    // Get the JSON
+    .getJSON()
+    // Once it's resolved, write out the JSON
+    .then(function (json) {
+        process.stdout.write(JSON.stringify(json));
+    });
