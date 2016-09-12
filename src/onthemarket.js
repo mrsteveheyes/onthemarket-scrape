7aebbec194226eb769fcfcdbf36a290b477ac725
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 11:56:57 +0100

    ADD: createParams function which passes test

--- src/onthemarket.js
+++ src/onthemarket.js
@@ -17,21 +17,7 @@ var OnTheMarket = function(area, min_price, max_price, type, min_bedrooms){
     this.min_price = min_price;
     this.max_price = max_price;
 
-    // Check the type parameter
-    switch(type){
-        // The list of excepted types
-        case "house":
-        case "flats-and-apartment":
-        case "bungalows":
-        case "land":
-            this.type  = type;
-            break;
-
-        // Default is just to use 'property'
-        default:
-            this.type  = "property";
-            break;
-    }
+    this.type  = type;
 
     this.min_bedrooms = min_bedrooms;
 };
@@ -43,7 +29,23 @@ var OnTheMarket = function(area, min_price, max_price, type, min_bedrooms){
  */
 OnTheMarket.prototype.createTypeSegment = function () {
     // Set initial segment to the type
-    var segment = this.type;
+    var segment = "";
+
+    // Check the type parameter
+    switch(this.type){
+        // The list of excepted types
+        case "house":
+        case "flats-and-apartment":
+        case "bungalows":
+        case "land":
+            segment = this.type;
+            break;
+
+        // Default is just to use 'property'
+        default:
+            segment = "property";
+            break;
+    }
 
     // Check to see if there is a min bedroom requirement
     if(this.min_bedrooms > 0) {
@@ -53,6 +55,53 @@ OnTheMarket.prototype.createTypeSegment = function () {
 
     // Return the segment
     return segment;
+};
+
+/**
+ * Create Params
+ *
+ * This function creates an array of the GET parameters that will be used in the request URL
+ */
+OnTheMarket.prototype.createParams = function () {
+    // Init the param array
+    var params = [];
+
+    // Create min-bedrooms params
+    params.push({
+        param: 'min-bedrooms',
+        value: this.min_bedrooms
+    });
+    // Create max price params
+    params.push({
+        param: 'max-price',
+        value: this.max_price
+    });
+    // Create min-price param
+    params.push({
+        param: 'min-price',
+        value: this.min_price
+    });
+
+    // Check if there is more than one type param
+    var types = this.type.split(',');
+    if(types.length > 1) {
+        // Create Property Type params
+        types.forEach(function (type) {
+            params.push({
+                param: "prop-types",
+                value: type
+            });
+        })
+    }
+
+    // Create radius param
+    params.push({
+        param: 'radius',
+        value: "1.0"
+    });
+
+    // Return the params
+    return params;
 };
 
 // Export the class
