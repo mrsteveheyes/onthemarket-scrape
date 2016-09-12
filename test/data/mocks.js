ee6d26cf2b779d09aa1906838e604003662a4949
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 21:04:46 +0100

    ADD: Abstracted out test data into separate file

--- test/data/mocks.js
+++ test/data/mocks.js
@@ -1,6 +1,78 @@
 "use strict";
 
 var mocks = {
+    createTypeSegementData: [
+        {args: ["Birmingham", 30000, 100000, "houses", 3], expected: '3-bed-houses'},
+        {args: ["Birmingham", 30000, 100000, "flats-apartments", 0], expected: 'flats-apartments'},
+    ],
+    optionTests: [
+        {arg: "houses", expected: "houses"},
+        {arg: "flats-apartments", expected: "flats-apartments"},
+        {arg: "bungalows", expected: "bungalows"},
+        {arg: "land", expected: "land"},
+        {arg: false, expected: "property"},
+        {arg: 400, expected: "property"},
+        {arg: "test", expected: "property"}
+    ],
+    paramTests: [
+        {
+            args: ["Area", 1, 2, "houses", 3],
+            expected: [
+                {
+                    param: "min-bedrooms",
+                    value: 3
+                },
+                {
+                    param: "max-price",
+                    value: 2
+                },
+                {
+                    param: "min-price",
+                    value: 1
+                },
+                {
+                    param: "radius",
+                    value: "1.0"
+                }
+            ]
+        },
+        {
+            args: ["Area", 1, 2, "houses,bungalows", 3],
+            expected: [
+                {
+                    param: "min-bedrooms",
+                    value: 3
+                },
+                {
+                    param: "max-price",
+                    value: 2
+                },
+                {
+                    param: "min-price",
+                    value: 1
+                },
+                {
+                    param: "prop-types",
+                    value: "houses"
+                },
+                {
+                    param: "prop-types",
+                    value: "bungalows"
+                },
+                {
+                    param: "radius",
+                    value: "1.0"
+                }
+            ]
+        }
+    ],
+    urlTests: [{
+        args: ["Area", 1, 2, "houses", 3],
+        expected: 'https://www.onthemarket.com/for-sale/3-bed-houses/area/?min-bedrooms=3&max-price=2&min-price=1&radius=1.0'
+    }, {
+        args: ["Area", 1, 2, "houses,bungalows", 3],
+        expected: 'https://www.onthemarket.com/for-sale/3-bed-property/area/?min-bedrooms=3&max-price=2&min-price=1&prop-types=houses&prop-types=bungalows&radius=1.0'
+    }],
     testJSON: {
         data: [
 
--- test/onethemarket.js
+++ test/onethemarket.js
@@ -10,10 +10,7 @@ describe('OnTheMarket', function () {
     describe('#createTypeSegement()', function () {
 
         // Set up test args and expected
-        var tests = [
-            {args: ["Birmingham", 30000, 100000, "houses", 3], expected: '3-bed-houses'},
-            {args: ["Birmingham", 30000, 100000, "flats-apartments", 0], expected: 'flats-apartments'},
-        ];
+        var tests = data.mocks.createTypeSegementData;
 
         // Create tests for each test cases
         tests.forEach(function (test) {
@@ -28,15 +25,7 @@ describe('OnTheMarket', function () {
             });
         });
 
-        var optionTests = [
-            {arg: "houses", expected: "houses"},
-            {arg: "flats-apartments", expected: "flats-apartments"},
-            {arg: "bungalows", expected: "bungalows"},
-            {arg: "land", expected: "land"},
-            {arg: false, expected: "property"},
-            {arg: 400, expected: "property"},
-            {arg: "test", expected: "property"}
-        ];
+        var optionTests = data.mocks.optionTests;
 
         optionTests.forEach(function (test) {
             it('Using type param "' + test.arg + '" returns the expected result of "' + test.expected + '"', function () {
@@ -55,58 +44,7 @@ describe('OnTheMarket', function () {
 
     describe('#createParams()', function () {
         // Param Test array
-        var paramsTest = [
-            {
-                args: ["Area", 1, 2, "houses", 3],
-                expected: [
-                    {
-                        param: "min-bedrooms",
-                        value: 3
-                    },
-                    {
-                        param: "max-price",
-                        value: 2
-                    },
-                    {
-                        param: "min-price",
-                        value: 1
-                    },
-                    {
-                        param: "radius",
-                        value: "1.0"
-                    }
-                ]
-            },
-            {
-                args: ["Area", 1, 2, "houses,bungalows", 3],
-                expected: [
-                    {
-                        param: "min-bedrooms",
-                        value: 3
-                    },
-                    {
-                        param: "max-price",
-                        value: 2
-                    },
-                    {
-                        param: "min-price",
-                        value: 1
-                    },
-                    {
-                        param: "prop-types",
-                        value: "houses"
-                    },
-                    {
-                        param: "prop-types",
-                        value: "bungalows"
-                    },
-                    {
-                        param: "radius",
-                        value: "1.0"
-                    }
-                ]
-            }
-        ];
+        var paramsTest = data.mocks.paramTests;
 
         paramsTest.forEach(function (test) {
             it('Creates the correct array for the params to be used in the URL using type of "' + test.args[3] + '"', function () {
@@ -123,13 +61,7 @@ describe('OnTheMarket', function () {
 
     describe('#createURL()', function () {
         // Create URL Test options
-        var urlTests = [{
-            args: ["Area", 1, 2, "houses", 3],
-            expected: 'https://www.onthemarket.com/for-sale/3-bed-houses/area/?min-bedrooms=3&max-price=2&min-price=1&radius=1.0'
-        }, {
-            args: ["Area", 1, 2, "houses,bungalows", 3],
-            expected: 'https://www.onthemarket.com/for-sale/3-bed-property/area/?min-bedrooms=3&max-price=2&min-price=1&prop-types=houses&prop-types=bungalows&radius=1.0'
-        }];
+        var urlTests = data.mocks.urlTests;
 
         // Run tests for each options
         urlTests.forEach(function (test) {
