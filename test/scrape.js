b00f60f11f3b338727bab755e97c3198ae822532
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-12 11:05:01 +0100

    ADD: Test for each type parameter returning the correct result

--- test/scrape.js
+++ test/scrape.js
@@ -4,26 +4,54 @@ var OnTheMarket = require('../src/onthemarket.js');
 
 describe('OnTheMarket', function() {
 
-    it('Create the correct type segment using only the type parameters', function(){
-        // Create test app
-        var testAppNoBedrooms = new OnTheMarket("Birmingham", 30000, 100000, "flats-and-apartment", 0);
-        // Call the createTypeSegement();
-        var resultNoBedrooms = testAppNoBedrooms.createTypeSegment();
+    describe('#createTypeSegement()', function(){
 
-        // Assert result
-        resultNoBedrooms.should.equal('flats-and-apartment');
+        // Set up test args and expected
+        var tests = [
+            {args: ["Birmingham", 30000, 100000, "house", 3],   expected: '3-bed-house'},
+            {args: ["Birmingham", 30000, 100000, "flats-and-apartment", 0],   expected: 'flats-and-apartment'},
+        ];
+
+        // Create tests for each test cases
+        tests.forEach(function(test) {
+            it('correctly builds segment to equal "' + test.expected + '"', function() {
+                // Create test app
+                var testApp = new OnTheMarket(test.args[0], test.args[1], test.args[2], test.args[3], test.args[4]);
+                // Call the createTypeSegement();
+                var result = testApp.createTypeSegment();
+
+                // Assert result
+                result.should.equal(test.expected);
+            });
+        });
+
+        var optionTests = [
+            {arg: "house", expected: "house"},
+            {arg: "flats-and-apartment", expected: "flats-and-apartment"},
+            {arg: "bungalows", expected: "bungalows"},
+            {arg: "land", expected: "land"},
+            {arg: false, expected: "property"},
+            {arg: 400, expected: "property"},
+            {arg: "test", expected: "property"}
+        ];
+
+        optionTests.forEach(function (test) {
+            it('Using type param "'+test.arg+'" returns the expected result of "'+test.expected+'"', function(){
+                var testApp = new OnTheMarket(null, null, null, test.arg, null);
+
+                // Call the createTypeSegement();
+                var result = testApp.createTypeSegment();
+
+                // Assert result
+                result.should.equal(test.expected);
+            });
+        });
+
+
 
     });
 
-    it('Create the correct type segment using both min bedroom and type parameters', function(){
-        // Create test app
-        var testApp = new OnTheMarket("Birmingham", 30000, 100000, "house", 3);
-        // Call the createTypeSegement();
-        var result = testApp.createTypeSegment();
 
-        // Assert result
-        result.should.equal('3-bed-house');
-    });
 
     //
     // it('Create the correct type segment', function(){
