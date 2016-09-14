a52d8f9045c8287d82a67135d4e1c1a172a37ab1
Steve Heyes <mrsteveheyes@gmail.com>
2016-09-14 15:42:33 +0100

    ADD: Better comments to index file

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ var OnTheMarket = require('./src/onthemarket.js');
 // Remove node and file path
 var params = process.argv.slice(2);
 
+// Check to see if the command is to listen
 if (params[0] == "listen") {
+    // Set up a blank app
     var app = new OnTheMarket();
+    // Subscribe to the channel
     app.subscribe();
 } else {
+    // Set up the app with the correct params
     var app = new OnTheMarket(params[0], params[1], params[2], params[3], params[4]);
 
     // Run the app
     app
-    // Get the JSON
+    // Get the HTML
         .getHTML()
-        // Once it's resolved, write out the JSON
         .then(function (html) {
+            // Once it's resolved, publish the HTML
             app.publish(html);
-
         })
         .catch(function (error) {
+            // Catch the errors and display them
             process.stdout.write(error.toString());
             process.exit();
         });
